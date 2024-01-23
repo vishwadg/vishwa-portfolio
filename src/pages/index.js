e4e5ab24b195ce93d6ea6ee0bbe75dd72a6e69f2
vishwa <vishwadeep.ghimire@gmail.com>
2024-01-22 19:41:57 -0600

    (feature): intro and about section addition

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Layout, Hero, About, Jobs, Featured, Education, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -9,7 +10,12 @@ const StyledMainContainer = styled.main`
 const IndexPage = ({ location }) => (
   <Layout location={location}>
     <StyledMainContainer className="fillHeight">
-     
+      <Hero />
+      <About />
+      <Jobs />
+      <Education/>
+      <Featured />
+      <Contact />
     </StyledMainContainer>
   </Layout>
 );
